17d79ed092877df8ccb33194a3bc3464c71ad2f6
chrisoney <christopher.oney@yahoo.com>
2021-09-17 15:45:46 -0700

    review, tag, and tagjoin model and migration

---
+++ backend/db/migrations/20210917222458-create-tag-join.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('TagJoins', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      storyId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'Stories' },
+        unique: 'tagjoins_unique'
+      },
+      tagId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'Tags' },
+        unique: 'tagjoins_unique'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      }
+    }, {
+      uniqueKeys: {
+        tagjoins_unique: {
+          attributes: ['storyId', 'tagId']
+        }
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('TagJoins');
+  }
+};
