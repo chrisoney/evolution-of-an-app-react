f8915cf6f785b48f600a2422c43ce7fab4546a00
chrisoney <christopher.oney@yahoo.com>
2021-09-17 15:07:37 -0700

    story model and migration

---
+++ backend/db/models/story.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Story = sequelize.define('Story', {
+    title: DataTypes.STRING,
+    author: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    pageCount: DataTypes.INTEGER,
+    linkUrl: DataTypes.STRING,
+    imageUrl: DataTypes.STRING,
+    warnings: DataTypes.JSON,
+    status: DataTypes.STRING
+  }, {});
+  Story.associate = function(models) {
+    // associations can be defined here
+  };
+  return Story;
+};
