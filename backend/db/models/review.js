b8c5433536b8f5c9ad9c4290f30629498f6f7c37
chrisoney <christopher.oney@yahoo.com>
2021-09-19 11:58:23 -0700

    update review model

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -1,13 +1,27 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Review = sequelize.define('Review', {
-    content: DataTypes.TEXT,
-    rating: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER,
-    storyId: DataTypes.INTEGER
+    content: {
+      type: DataTypes.TEXT,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isNumeric: true,
+        max: 5,
+        min: 1,
+      }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+    },
+    storyId: {
+      type: DataTypes.INTEGER,
+    }
   }, {});
   Review.associate = function(models) {
-    // associations can be defined here
+    Review.belongsTo(models.User, { foreignKey: 'userId' })
+    Review.belongsTo(models.Story, { foreignKey: 'storyId' })
   };
   return Review;
 };
