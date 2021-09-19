1643b473f89c11d93df8314caf4300ac98d7a7bd
chrisoney <christopher.oney@yahoo.com>
2021-09-19 11:57:16 -0700

    update bookshelf model

--- backend/db/models/bookshelf.js
+++ backend/db/models/bookshelf.js
@@ -2,11 +2,27 @@
 module.exports = (sequelize, DataTypes) => {
   const Bookshelf = sequelize.define('Bookshelf', {
     userId: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    deleteAllowed: DataTypes.BOOLEAN
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1, 30]
+      }
+    },
+    deleteAllowed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
   }, {});
   Bookshelf.associate = function(models) {
-    // associations can be defined here
+    Bookshelf.belongsTo(models.User, { foreignKey: 'userId' });
+    Bookshelf.hasMany(models.Placement, { foreignKey: 'bookshelfId', onDelete: 'CASCADE', hooks: true })
+
+    const columnMapping = {
+      through: 'Placement',
+      foreignKey: 'bookshelfId',
+      otherKey: 'storyId'
+    }
+    Bookshelf.belongsToMany(models.Story, columnMapping)
   };
   return Bookshelf;
 };
