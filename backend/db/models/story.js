c5d072fa51d73e7d7ea390a898d665c8efdb5316
chrisoney <christopher.oney@yahoo.com>
2021-09-19 11:58:47 -0700

    update story model

--- backend/db/models/story.js
+++ backend/db/models/story.js
@@ -11,7 +11,23 @@ module.exports = (sequelize, DataTypes) => {
     status: DataTypes.STRING
   }, {});
   Story.associate = function(models) {
-    // associations can be defined here
+    Story.hasMany(models.Placement, { foreignKey: 'storyId', onDelete: 'CASCADE', hooks: true })
+    
+    Story.hasMany(models.Review, { foreignKey: 'storyId', onDelete: 'CASCADE', hooks: true })
+    
+    const columnMapping = {
+      through: 'Placement',
+      foreignKey: 'storyId',
+      otherKey: 'bookshelfId'
+    }
+    Story.belongsToMany(models.Bookshelf, columnMapping);
+
+    const columnMapping2 = {
+      through: 'TagJoin',
+      foreignKey: 'storyId',
+      otherKey: 'tagId'
+    }
+    Story.belongsToMany(models.Tag, columnMapping2);
   };
   return Story;
 };
