7d18946f009d70a4117eb489aeb22980ac614b90
chrisoney <christopher.oney@yahoo.com>
2021-09-21 15:06:42 -0700

    finish auth refactor of the login page

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
-import './LoginForm.css';
+import styles from './loginForm.module.css';
 
 function LoginFormPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
+  const stage = useSelector((state) => state.ui.stage)
   const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
@@ -22,7 +23,12 @@ function LoginFormPage() {
       });
   };
 
-  return (
+  const handleDemo = async (e) => {
+    e.preventDefault()
+    await dispatch(sessionActions.demo())
+  }
+
+  return stage === 0 ? (
     <>
       <h1>Log In</h1>
       <form onSubmit={handleSubmit}>
@@ -52,7 +58,58 @@ function LoginFormPage() {
         <button type="submit">Log In</button>
       </form>
     </>
-  );
+  ) : (
+      <>
+        <a href='/' className={styles.logo_container}>
+          <img src='/images/logo.png' className={styles.auth_logo} />
+        </a>
+        <div className={styles.form_container}>
+          <h1 className={styles.auth_title}>Sign in to RoyalReads</h1>
+          <ul>
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
+            ))}
+          </ul>
+          <form>
+            <div className={styles.form_group}>
+              <label className={styles.form_label}>Username or Email</label>
+              <input
+                type="text"
+                value={credential}
+                className={styles.form_control}
+                onChange={(e) => setCredential(e.target.value)}
+                required
+              />
+            </div>
+            <div className={styles.form_group}>
+              <label className={styles.form_label}>Password</label>
+              <input
+                type="password"
+                value={password}
+                className={styles.form_control}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className={styles.form_buttons}>
+              <button
+                className={styles.positive_button}
+              >Sign in</button>
+              <button
+                className={styles.positive_button}
+                onClick={handleDemo}
+              >Demo</button>
+              <a href='/' className={styles.neutral_link}>Cancel</a>
+            </div>
+          </form>
+          <div className={styles.auth_bottom_nav}>
+            <span>Not a member?</span>
+            <a href="/signup" className={styles.neutral_link}>Sign up</a>
+          </div>
+        </div>
+      
+      </>
+  )
 }
 
 export default LoginFormPage;
