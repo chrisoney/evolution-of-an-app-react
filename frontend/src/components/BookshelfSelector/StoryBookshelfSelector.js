4182fc279139a748795ff837c802fa203b161233
chrisoney <christopher.oney@yahoo.com>
2021-09-30 21:15:58 -0700

    start on bookshelf selector

--- frontend/src/components/BookshelfSelector/StoryBookshelfSelector.js
+++ frontend/src/components/BookshelfSelector/StoryBookshelfSelector.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styles from './bookshelfSelector.module.css';
+
 // Work on this next
 const StoryBookshelfSelector = () => {
-  return null;
+  const dispatch = useDispatch()
+  const sessionUser = useSelector(state => state.session.user);
+  const [shelved, setShelved] = useState(false)
+
+  return (
+    <>
+      
+    </>
+  );
 }
 
 export default StoryBookshelfSelector;
+
+// if story.Bookshelves.length > 0
+//   div(class='bookshelf-button-container added')
+//     i(class='fas fa-check')
+//     div(class='bookshelf-button-added' id=story.Bookshelves[0].id) #{story.Bookshelves[0].name}
+// else
+//   div(class='bookshelf-button-container')
+//     div.placeholder
+//     div(class='bookshelf-button' id=wantToReadId) Want To Read
+//     i(class='fas fa-chevron-down')
