d45cd72a2112f81720f11136fbea3f5d5612e1da
chrisoney <christopher.oney@yahoo.com>
2021-10-12 09:11:02 -0700

    fill out delete modal

--- frontend/src/components/Modal/BookshelfSelectorRemoveModal.js
+++ frontend/src/components/Modal/BookshelfSelectorRemoveModal.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { fetchAllPlacements } from '../../store/placements';
+import { hideModal } from '../../store/ui';
 
 import styles from './storyModal.module.css'
 
@@ -15,7 +16,26 @@ const BookshelfSelectorRemoveModal = () => {
 
   return (
     <div className={styles.modal_container}>
-      <div className={styles.modal_title_container}></div>
+      <div className={styles.modal_title_container}>
+        <div className={styles.modal_title}>Are you sure you want to remove this book from your shelves? </div>
+        <div
+          className={styles.close_button_container}
+          onClick={(e) => dispatch(hideModal())}
+        >
+          <i className={`fas fa-times close-modal ${styles.fa_times} ${styles.close_modal}`} />
+        </div>
+      </div>
+      <div className={styles.modal_description}>Removing this book will clear associated reading activity.</div>
+      <div className={styles.modal_bottom_button_container}>
+        <button
+          className={styles.modal_cancel}
+          onClick={null} // Use to switch back to modal 1?
+        >Cancel</button>
+        <button
+          className={styles.modal_submit}
+          onClick={null} // This will be an actual action to delete it from existing bookshelves
+        >Remove</button>
+      </div>
     </div>
   );
 }
