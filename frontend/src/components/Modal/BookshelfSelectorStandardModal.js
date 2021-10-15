a94aeb636e5a1a594b7933a785479aa52f7b9df4
chrisoney <christopher.oney@yahoo.com>
2021-10-14 18:12:15 -0700

    add switch to custom bookshelf modal

--- frontend/src/components/Modal/BookshelfSelectorStandardModal.js
+++ frontend/src/components/Modal/BookshelfSelectorStandardModal.js
@@ -6,6 +6,7 @@ import { fetchAllBookshelves } from '../../store/bookshelves'
 import { hideModal, setCurrentModal } from '../../store/ui';
 
 import BookshelfSelectorRemoveModal from './BookshelfSelectorRemoveModal';
+import BookshelfSelectorCustomModal from './BookshelfSelectorCustomModal';
 
 import styles from './storyModal.module.css';
 
@@ -49,8 +50,13 @@ const BookshelfSelectorStandardModal = () => {
 
   const handleRemoveModalSwitch = (e) => {
     e.preventDefault()
-    e.stopPropagation()
+    // e.stopPropagation()
     dispatch(setCurrentModal(BookshelfSelectorRemoveModal));
+  }
+  const handleCustomModalSwitch = (e) => {
+    e.preventDefault()
+    // e.stopPropagation()
+    dispatch(setCurrentModal(BookshelfSelectorCustomModal));
   }
 
   if (!props.largeVersion || !props.storyId) return <p>Did something go wrong?</p>
@@ -91,7 +97,7 @@ const BookshelfSelectorStandardModal = () => {
           >Remove</button>
           <button
             className={styles.modal_submit}
-            // onClick={null} // This will be movement to the custom shelves modal
+            onClick={handleCustomModalSwitch} // This will be movement to the custom shelves modal
           >Next</button>
         </div>
       )}
@@ -101,81 +107,3 @@ const BookshelfSelectorStandardModal = () => {
 }
 
 export default BookshelfSelectorStandardModal;
-
-//   const titleContainer = document.createElement('div')
-//   titleContainer.className = 'modal-title-container';
-//   const title = document.createElement('div');
-//   title.className = 'modal-title';
-//   title.innerText = 'Choose a shelf for this story: ';
-//   titleContainer.appendChild(title);
-//   const closeButtonContainer = document.createElement('i');
-//   closeButtonContainer.className = 'close-button-container';
-//   closeButtonContainer.addEventListener('click', closeModal)
-//   const closeButton = document.createElement('i');
-//   closeButton.className = 'fas fa-times close-modal';
-//   closeButtonContainer.appendChild(closeButton);
-//   titleContainer.appendChild(closeButtonContainer);
-//   result.push(titleContainer);
-  
-//   const wantToRead = document.createElement('button');
-//   wantToRead.className = 'modal-bookshelf-button';
-//   wantToRead.id = data['Want To Read'].id;
-//   wantToRead.innerText = 'Want To Read';
-//   if (large) {
-//     wantToRead.addEventListener('click', switchShelfHelperEvent)
-//     if( data['Want To Read'].Stories.length > 0) {
-//       wantToRead.classList.add('selected');
-//       wantToRead.innerHTML = '<i class="fas fa-check"></i>' + wantToRead.innerHTML;
-//     }
-//   }  else {
-//     wantToRead.addEventListener('click', selectBookshelf)
-//   }
-//   result.push(wantToRead);
-
-//   const currentlyReading = document.createElement('button');
-//   currentlyReading.className = 'modal-bookshelf-button';
-//   currentlyReading.id = data['Currently Reading'].id;
-//   currentlyReading.innerText = 'Currently Reading';
-//   if (large) {
-//     currentlyReading.addEventListener('click', switchShelfHelperEvent)
-//     if (data['Currently Reading'].Stories.length > 0) {
-//       currentlyReading.classList.add('selected');
-//       currentlyReading.innerHTML = '<i class="fas fa-check"></i>' + currentlyReading.innerHTML;
-//     }
-//   } else {
-//     currentlyReading.addEventListener('click', selectBookshelf)
-//   }
-//   result.push(currentlyReading);
-
-//   const alreadyRead = document.createElement('button');
-//   alreadyRead.className = 'modal-bookshelf-button';
-//   alreadyRead.id = data['Read'].id;
-//   alreadyRead.innerText = 'Read';
-//   if (large) {
-//     alreadyRead.addEventListener('click', switchShelfHelperEvent)
-//     if(data['Read'].Stories.length > 0) {
-//       alreadyRead.classList.add('selected');
-//       alreadyRead.innerHTML = '<i class="fas fa-check"></i>' + alreadyRead.innerHTML;
-//     }
-//   } else {
-//     alreadyRead.addEventListener('click', selectBookshelf)
-//   }
-//   result.push(alreadyRead);
-
-//   if (large) {
-//     const bottomButtonContainer = document.createElement('div');
-//     bottomButtonContainer.className = 'modal-bottom-button-container';
-//     const cancelButton = document.createElement('button');
-//     cancelButton.className = 'modal-cancel';
-//     cancelButton.innerText = 'Remove';
-//     cancelButton.addEventListener('click', removeAllWarningEvent);
-//     bottomButtonContainer.appendChild(cancelButton);
-//     const submitButton = document.createElement('button');
-//     submitButton.className = 'modal-submit';
-//     submitButton.innerText = 'Next';
-//     submitButton.addEventListener('click', customShelfModalDisplayEvent);
-//     bottomButtonContainer.appendChild(submitButton);
-//     result.push(bottomButtonContainer);
-//   }
-
-//   return result;
