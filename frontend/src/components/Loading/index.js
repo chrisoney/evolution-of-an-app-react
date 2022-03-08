7fbed4a259261ac528d2d10f89c8667f1eea4228
chrisoney <christopher.oney@yahoo.com>
2022-03-08 15:49:22 -0800

    add spinner logo

--- frontend/src/components/Loading/index.js
+++ frontend/src/components/Loading/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 const Loading = () => {
   return (
     <>
-      <img src='https://www.behance.net/gallery/31234507/Open-source-Loading-GIF-Icons-Vol-1/modules/199929399' />
+      <img src='/images/spinner.svg' />
     </>
   )
 }
